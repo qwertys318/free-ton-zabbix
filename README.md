0dbc493d95d3974aaa5fd559874730d07f02dc5f
Maxim Salamakhin <343604@gmail.com>
2020-05-18 16:13:24 +0700

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Free TON zabbix templates
+
+##Installation
+```
+git clone https://github.com/SalamAkhin/free-ton-zabbix.git
+sudo cp -r ./free-ton-zabbix/etc/zabbix/* /etc/zabbix/
+```
+
+Set <<_TON_BUILD_DIR_>> <<_KEYS_DIR_>> in
+```
+vim /etc/zabbix/zabbix_agent2.d/userparameter_freeton_node.conf
+
+sudo systemctl restart zabbix-agent2
+```
+
+Import template into your zabbix server and add it to host.
+
+
+>>Suggestions for new metrics are welcome to telegram [@qwertys318](https://t.me/qwertys318)<<
---
+++ etc/zabbix/scripts/freeton_node_time_diff.sh
@@ -0,0 +1,22 @@
+engineConsole=${1}/validator-engine-console/validator-engine-console
+keyDir=${2}
+
+if ! test -f "$engineConsole"; then
+  echo "Console \"$engineConsole\" not available"
+  exit 1
+fi
+
+if [ ! -d "$keyDir" ]; then
+  echo "Key folder \"$keyDir\" not available"
+  exit 1
+fi
+
+data=$( "$engineConsole" -a 127.0.0.1:3030 -k "$keyDir/client" -p "$keyDir/server.pub" -c "getstats" -c "quit" )
+
+unixtime=$(echo $data | grep -oP '(?<=unixtime\s)\d+')
+masterchainblocktime=$(echo $data | grep -oP '(?<=masterchainblocktime\s)\d+')
+time_diff=$((masterchainblocktime-unixtime))
+
+echo $time_diff
+
+exit 0
